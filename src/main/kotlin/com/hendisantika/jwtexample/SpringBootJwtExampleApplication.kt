8e87ae2af8ee19e2b388561454e66a1cf68a8cac
Hendi Santika <hendisantika@gmail.com>
2022-02-07 12:42:45 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/jwtexample/SpringBootJwtExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.jwtexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJwtExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootJwtExampleApplication>(*args)
+}
