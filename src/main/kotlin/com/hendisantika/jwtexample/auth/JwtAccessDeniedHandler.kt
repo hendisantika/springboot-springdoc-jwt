33a2e26b5e195f44e7e44bf5f6f3ab33a45f3f6c
Hendi Santika <hendisantika@gmail.com>
2022-02-07 12:55:09 +0700

    #9 - Add JwtAccessDeniedHandler

---
+++ src/main/kotlin/com/hendisantika/jwtexample/auth/JwtAccessDeniedHandler.kt
@@ -0,0 +1,16 @@
+package com.hendisantika.jwtexample.auth
+
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtAccessDeniedHandler : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        accessDeniedException: AccessDeniedException
+    ) {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.message)
+    }
+}
