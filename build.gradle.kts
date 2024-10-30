34e53d9204b51e88e801102412f89f169b523b0b
Hendi Santika <hendisantika@gmail.com>
2024-10-30 11:16:22 +0700

    Use JDK 21🫸🌀✏️📗🐧🐳⬆
    Use JDK 21🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
 }
